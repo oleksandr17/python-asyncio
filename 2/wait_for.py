1a0e350c4c28215fbe607cf3471dbce5a85cd08f
oleksandr17 <berezhnyi@gmail.com>
2020-09-18 19:32:13 +0200

    More examples

---
+++ 2/wait_for.py
@@ -0,0 +1,15 @@
+import asyncio
+import time
+
+async def eternity():
+    # Sleep for one hour
+    await asyncio.sleep(3600)
+
+async def main():
+    try:
+        # Wait for at most 1 second
+        await asyncio.wait_for(eternity(), timeout=1.0) # creates a new task for 'eternity'
+    except asyncio.TimeoutError:
+        print('timeout')
+
+asyncio.run(main())
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 References:
-- [Async IO in Python: A Complete Walkthrough](https://realpython.com/async-io-python)
+1. [Async IO in Python: A Complete Walkthrough](https://realpython.com/async-io-python)
+1. [Coroutines and Tasks](https://docs.python.org/3/library/asyncio-task.html)
